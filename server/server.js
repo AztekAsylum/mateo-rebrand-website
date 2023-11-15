a8831ec4820e1b046f4fcf7eb0208cf715c12ec7
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-11-15 16:00:45 -0700

    built basic server and vite front-end

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.sendFile("../client/index.html");
+  });
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
