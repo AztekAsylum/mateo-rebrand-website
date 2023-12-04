68f92492a2c10f2ddafd5036b558fb88d9af24d8
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-12-04 13:15:14 -0700

    updated about me homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,8 +6,18 @@ const Home = () => {
     <Wrapper>
       <Container id="HomePageContainer">
         <Row className="">
-          <Col xs={12} className="mx-auto">
-            <p className="display-1 mx-auto text-center">Home Page</p>
+          <Col xs={12} className="mx-auto text-white">
+            <p className="display-0 mx-auto text-center">
+              Front End Developer, earning praise for his creative flair merging
+              graphic design finesse with a skill for crafting polished and
+              captivating front-end interfaces. Rooted in graphic design and
+              honed expertise, he curates exceptional content tailored for
+              promotions and engaging audiences. Proficient across graphic
+              design, photography, and videography, Mateo produces unique and
+              compelling material. His greatest strengths shine in
+              collaboration, displaying a strong work ethic and adaptability
+              throughout the creative process.
+            </p>
           </Col>
         </Row>
       </Container>
